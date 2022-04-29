2dab5d3e13564c7d2908ebe49514aa5a6564f36a
YoungWonius <59616376+YoungWonius@users.noreply.github.com>
2022-04-29 11:54:22 +0900

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,31 @@
+// Goal: Create a reusable function for getting the forecast
+//
+// 1. Setup the "forecast" function in utils/forecast.js
+// 2. Require the function in app.js and call it as shown below
+// 3. The forecast function should have three potential calls to callback:
+//    - Low level error, pass string for error
+//    - Coordinate error, pass string for error
+//    - Success, pass forecast string for data (same format as from before)
+const request = require("request");
+
+const forecast = (latitude, longitude, callback) => {
+  const url = `http://api.weatherstack.com/current?access_key=6298ecd354ad73ffa46c0c0f41f1ff66&query=${latitude},${longitude}&units=f`;
+
+  request({ url: url, json: true }, (error, response) => {
+    if (error) {
+      callback("Unable to connect to weather service!", undefined);
+    } else if (response.body.error) {
+      callback("Unable to find location", undefined);
+    } else {
+      callback(
+        undefined,
+        response.body.current.weather_descriptions[0] +
+          ". It is currently " +
+          response.body.current.temperature +
+          " degress out."
+      );
+    }
+  });
+};
+
+module.exports = forecast;
